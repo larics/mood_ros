a6f83a99b04f4ed3c17c4021c5236d2eaab3b35e
Lovro Markovic <lovro.markovic@fer.hr>
2021-02-23 22:09:44 +0100

    [blob_detector] Initialize SimpleBlobDetector

--- src/blob_detector.cpp
+++ src/blob_detector.cpp
@@ -1,15 +1,22 @@
 #include <ros/ros.h>
 #include <mood_ros/BlobDetectorParamsConfig.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/CompressedImage.h>
+#include <sensor_msgs/Image.h>
 #include <mood_ros/detector_interface.hpp>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 namespace mood_plugin {
 
 class BlobDetector : public mood_base::detector_interface
 {
 public:
-  BlobDetector() { ROS_INFO("[BlobDetector] Constructor"); }
+  BlobDetector() : m_blob_detector_ptr(cv::SimpleBlobDetector::create())
+  {
+    ROS_INFO("[BlobDetector] Constructor");
+  }
 
   sensor_comm::detection_response update(
     const sensor_comm::sensor_info &sensor_info) override
@@ -32,18 +39,24 @@ public:
 
     ROS_INFO_THROTTLE(5.0, "[BlobDetector] Update");
 
+    // Convert the image to OpencCV
+    auto cv_image_ptr =
+      cv_bridge::toCvCopy(sensor_info.rgb_image, sensor_msgs::image_encodings::BGR8);
+
+    // Do the Blob Detection
+    std::vector<cv::KeyPoint> keypoints;
+    m_blob_detector_ptr->detect(cv_image_ptr->image, keypoints);
+
     return { true, "[BlobDetector] Update successful." };
   }
 
   geometry_msgs::PoseArray get_object_poses() override
   {
-    ROS_INFO("[BlobDetector] get_object_poses");
     return {};
   }
 
   sensor_msgs::Image get_labeled_image() override
   {
-    ROS_INFO("[BlobDetector] get_labeled_image");
     return {};
   }
 
@@ -56,6 +69,7 @@ public:
 
 private:
   bool m_is_initialized = false;
+  cv::Ptr<cv::SimpleBlobDetector> m_blob_detector_ptr;
 };
 }// namespace mood_plugin
 
