0fde10ba13e73b93f8316a20eaa2b5b0bdde3cd3
Lovro Markovic <lovro.markovic@fer.hr>
2021-02-24 23:20:41 +0100

    [BlobDetector] Detect Blob poses

--- src/blob_detector.cpp
+++ src/blob_detector.cpp
@@ -1,9 +1,11 @@
 #include <memory>
+#include <cmath>
+
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/Image.h>
+#include <tf2/LinearMath/Matrix3x3.h>
 
-#include <mood_ros/BlobDetectorParamsConfig.h>
 #include <mood_ros/detector_interface.hpp>
 
 // OpenCV Includes
@@ -39,9 +41,11 @@ class BlobDetector : public mood_base::detector_interface
 public:
   BlobDetector()
     : m_blob_detector_ptr(cv::SimpleBlobDetector::create()),
-      m_cloud_ptr(boost::make_shared<Pointcloud_t>())
+      m_cloud_ptr(boost::make_shared<Pointcloud_t>()),
+      m_cvimage_ptr(boost::make_shared<cv_bridge::CvImage>())
   {
     ROS_INFO("[BlobDetector] Constructor");
+    m_camera_to_base_link.setRPY(-1.57, 0, -1.57);
   }
 
   sensor_comm::detection_response update(
@@ -91,7 +95,12 @@ public:
     return { true, "[BlobDetector] Update successful." };
   }
 
-  geometry_msgs::PoseArray get_object_poses() override { return {}; }
+  geometry_msgs::PoseArray get_object_poses() override
+  {
+    m_blob_poses.header.stamp = ros::Time::now();
+    m_blob_poses.header.frame_id = "red/base_link";
+    return m_blob_poses;
+  }
 
   sensor_msgs::Image get_labeled_image() override
   {
@@ -124,10 +133,46 @@ private:
       cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
   }
 
-  geometry_msgs::Pose compute_blob_sentroid() {}
+  std::tuple<geometry_msgs::Pose, bool> compute_blob_centroid(
+    const cv::Mat &one_blob_mask)
+  {
+    tf2::Vector3 blob_pose{0, 0, 0};
+    int count = 0;
+    for (int i = 0; i < one_blob_mask.rows; i++) {
+      for (int j = 0; j < one_blob_mask.cols; j++) {
+        if (one_blob_mask.at<unsigned char>(i, j) == 0) { continue; }
+
+        count++;
+        auto point = m_cloud_ptr->at(j, i);
+        blob_pose += tf2::Vector3{point.x, point.y, point.z};
+      }
+    }
+
+    blob_pose /= count;
+
+    if (!(std::isfinite(blob_pose.getX()) || std::isfinite(blob_pose.getY())
+          || std::isfinite(blob_pose.getZ()))) {
+      return { geometry_msgs::Pose{}, false };
+    }
+
+    blob_pose = m_camera_to_base_link * blob_pose;
+
+    geometry_msgs::Pose new_blob_pose;
+    new_blob_pose.position.x = blob_pose.x();
+    new_blob_pose.position.y = blob_pose.y();
+    new_blob_pose.position.z = blob_pose.z();
+    new_blob_pose.orientation.x = 0;
+    new_blob_pose.orientation.y = 0;
+    new_blob_pose.orientation.z = 0;
+    new_blob_pose.orientation.w = 1;
+    return { new_blob_pose, true };
+  }
 
   void compute_blob_centroids(int image_rows, int image_cols)
   {
+    // Clear current blob poses
+    m_blob_poses.poses.clear();
+
     // Go through all keypoints and find out their position
     cv::Mat debug_mask(image_rows, image_cols, CV_8UC1, Color::BLACK);
     for (const auto &keypoint : m_blob_keypoints) {
@@ -136,16 +181,20 @@ private:
       // Draw a circle for the debug mask
       cv::circle(debug_mask,
         cv::Point(keypoint.pt.x, keypoint.pt.y),
-        keypoint.size / 2.0,// keypoint.size is a diameter, not a radius. Duh...
+        keypoint.size / 3.0,// keypoint.size is a diameter, not a radius. Duh...
         Color::WHITE,
         -1);
 
       // Draw a circle for the one blob mask
       cv::circle(one_blob_mask,
         cv::Point(keypoint.pt.x, keypoint.pt.y),
-        keypoint.size / 5.0,
+        keypoint.size / 3.0,
         Color::WHITE,
         -1);
+
+      auto [blob_pose, success] = compute_blob_centroid(one_blob_mask);
+      if (!success) { continue; }
+      m_blob_poses.poses.emplace_back(blob_pose);
     }
 
     cv_bridge::CvImage debug_mask_cvimage;
@@ -156,11 +205,13 @@ private:
   }
 
   bool m_is_initialized = false;
+  tf2::Matrix3x3 m_camera_to_base_link;
   cv::Ptr<cv::SimpleBlobDetector> m_blob_detector_ptr;
 
   /* Blob label information */
   std::vector<cv::KeyPoint> m_blob_keypoints;
   cv::Mat m_labeled_image;
+  geometry_msgs::PoseArray m_blob_poses;
 
   /* Image transport for debugging */
   std::unique_ptr<image_transport::ImageTransport> m_it_ptr;
